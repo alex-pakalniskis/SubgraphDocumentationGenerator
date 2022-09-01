e543882d0120ddfbd3ea14b9edb78a313ed90025
Alex Pakalniskis 3 <alexpakalniskis@gmail.com>
2022-09-01 07:56:11 -0700

    add table frame to overview

--- overview.md
+++ overview.md
@@ -4,3 +4,7 @@ Everest has a GraphQL API endpoint hosted by [The Graph](https://thegraph.com/do
 ## Official subgraphs
 Official subgraphs are subgraphs maintained by the Everest team.
 
+| Chain | Explorer Page | API Endpoint |
+| --- | --- | --- |
+
+* An [API key](https://thegraph.com/docs/en/querying/managing-api-keys/) needed is needed to query our Ethereum subgraph, as it's based on [The Graph](https://thegraph.com/)'s decentralized network. Replace [api-key] with your API key in the API endpoint. [Here](https://thegraph.com/docs/en/studio/managing-api-keys/) is a good guide on how to manage your API keys and set indexer preferences.
--- src/classes.py
+++ src/classes.py
@@ -35,6 +35,14 @@ class OverviewPage:
         self.markdown += f"{project_name} has a GraphQL API endpoint hosted by [The Graph](https://thegraph.com/docs/about/introduction#what-the-graph-is) called a subgraph for indexing and organizing data from the Everest smart contracts. The code for our subgraph(s) can be found [here]({self.repository}).\n\n"
         self.markdown += f"## Official subgraphs\n"
         self.markdown += f"Official subgraphs are subgraphs maintained by the {project_name} team.\n\n"
+        self.markdown += f"| Chain | Explorer Page | API Endpoint |\n"
+        self.markdown += f"| --- | --- | --- |\n"
+        # loop through chains
+        # for source in self.data_sources:
+        #     self.markdown += f"| {source.data_sources}"
+
+
+        self.markdown += f"\n* An [API key](https://thegraph.com/docs/en/querying/managing-api-keys/) needed is needed to query our Ethereum subgraph, as it's based on [The Graph](https://thegraph.com/)'s decentralized network. Replace [api-key] with your API key in the API endpoint. [Here](https://thegraph.com/docs/en/studio/managing-api-keys/) is a good guide on how to manage your API keys and set indexer preferences."
         
 
     def save(self):
